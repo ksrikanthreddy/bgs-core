8cb2d04ebf9410f7b16b31456c3c0f23cade2e91
Srikanth Reddy <kollisreekanth@gmail.com>
2015-01-06 14:31:52 +0530

    Update PowerConnectionReceiver.java

--- src/android/PowerConnectionReceiver.java
+++ src/android/PowerConnectionReceiver.java
@@ -1,3 +1,9 @@
+package com.red_folder.phonegap.plugin.backgroundservice;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
 public class PowerConnectionReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) { 
