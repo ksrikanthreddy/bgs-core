c9800ce14fdb75265e3545ef50e3afac2dc69b46
Srikanth Reddy <kollisreekanth@gmail.com>
2015-01-07 14:24:32 +0530

    Update PowerConnectionReceiver.java

--- src/android/PowerConnectionReceiver.java
+++ src/android/PowerConnectionReceiver.java
@@ -9,7 +9,7 @@ import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.R;
-
+import android.widget.Toast;
 
 
 public class PowerConnectionReceiver extends BroadcastReceiver {
