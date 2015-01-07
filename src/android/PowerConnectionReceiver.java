1930a3935064e4595168d806ca3668ecc7951f25
Srikanth Reddy <kollisreekanth@gmail.com>
2015-01-07 18:06:57 +0530

    Update PowerConnectionReceiver.java

--- src/android/PowerConnectionReceiver.java
+++ src/android/PowerConnectionReceiver.java
@@ -14,37 +14,50 @@ import android.widget.Toast;
 
 public class PowerConnectionReceiver extends BroadcastReceiver {
     
-    @Override
-    public void onReceive(Context context, Intent intent) { 
-        Toast toast1 = Toast.makeText(context, "started", Toast.LENGTH_SHORT);
-        toast1.show();
-        try{
-        int status = intent.getIntExtra(BatteryManager.EXTRA_STATUS, -1);
-        boolean isCharging = status == BatteryManager.BATTERY_STATUS_CHARGING ||
-                            status == BatteryManager.BATTERY_STATUS_FULL;
-    
-        int chargePlug = intent.getIntExtra(BatteryManager.EXTRA_PLUGGED, -1);
-        boolean usbCharge = chargePlug == BatteryManager.BATTERY_PLUGGED_USB;
-        boolean acCharge = chargePlug == BatteryManager.BATTERY_PLUGGED_AC;
-        final NotificationManager mgr=
+    public void showNotification(Context context, String title, String description){
+    	final NotificationManager mgr=
                 (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
             Notification note=new Notification(R.drawable.star_big_on,
-                                                            "Android Example Status message!",
+                                                            title,
                                                             System.currentTimeMillis());
 		Intent contentIntent = new Intent();
              PendingIntent appIntent = PendingIntent.getActivity(context, 0, contentIntent, 0);
-	        note.setLatestEventInfo(context, "1", "2", appIntent);
-
+	        note.setLatestEventInfo(context, title, description, appIntent);
             //After uncomment this line you will see number of notification arrived
             //note.number=2;
-            mgr.notify(100, note);
+            mgr.notify(100, note);	
+    
+    }
+    @Override
+    public void onReceive(Context context, Intent intent) { 
+        Toast toast1 = Toast.makeText(context, "Safe Battery Enabled", Toast.LENGTH_SHORT);
+        toast1.show();
+        try{
+       	String batteryStatus = "";
+        int status = intent.getIntExtra(BatteryManager.EXTRA_STATUS, -1);
+        boolean isCharging = status == BatteryManager.BATTERY_STATUS_CHARGING;
+    	boolean isFull = status == BatteryManager.BATTERY_STATUS_FULL;
+        int chargePlug = intent.getIntExtra(BatteryManager.EXTRA_PLUGGED, -1);
+        boolean usbCharge = chargePlug == BatteryManager.BATTERY_PLUGGED_USB;
+        boolean acCharge = chargePlug == BatteryManager.BATTERY_PLUGGED_AC;
+        if(usbCharge)
+        {
+        	batteryStatus = "USB";
+        }
+        if(acCharge)
+        {
+        	batteryStatus = "AC Power";
+        }
+        if(isCharging){
+        	showNotification(context,"Safe Battery", batteryStatus + " Charging.");
+        }
+        if(isFull){
+        	showNotification(context,"Safe Battery", "100% charged. Unplug Charger.");
+        }
         }
         catch(Exception e){
-        	Toast toast2 = Toast.makeText(context, "exception occured", Toast.LENGTH_SHORT);
-            toast2.show();
             Toast toast = Toast.makeText(context, e.getMessage(), Toast.LENGTH_SHORT);
             toast.show();
-            
         }
     }
 }
