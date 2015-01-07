25e569abc49a82980b0cc63bc8af83b9366fb799
Srikanth Reddy <kollisreekanth@gmail.com>
2015-01-07 18:15:31 +0530

    Update PowerConnectionReceiver.java

--- src/android/PowerConnectionReceiver.java
+++ src/android/PowerConnectionReceiver.java
@@ -15,6 +15,7 @@ import android.widget.Toast;
 public class PowerConnectionReceiver extends BroadcastReceiver {
     
     public void showNotification(Context context, String title, String description){
+    	try{
     	final NotificationManager mgr=
                 (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
             Notification note=new Notification(R.drawable.star_big_on,
@@ -26,7 +27,11 @@ public class PowerConnectionReceiver extends BroadcastReceiver {
             //After uncomment this line you will see number of notification arrived
             //note.number=2;
             mgr.notify(100, note);	
-    
+    	}
+    	catch(Exception e){
+    		Toast toast = Toast.makeText(context, e.getMessage(), Toast.LENGTH_SHORT);
+            	toast.show();
+    	}
     }
     @Override
     public void onReceive(Context context, Intent intent) { 
@@ -35,7 +40,7 @@ public class PowerConnectionReceiver extends BroadcastReceiver {
         try{
        	String batteryStatus = "";
         int status = intent.getIntExtra(BatteryManager.EXTRA_STATUS, -1);
-        boolean isCharging = status == BatteryManager.BATTERY_STATUS_CHARGING;
+        boolean isCharging = status == BatteryManager.BATTERY_STATUS_CHARGING || BatteryManager.BATTERY_STATUS_FULL;
     	boolean isFull = status == BatteryManager.BATTERY_STATUS_FULL;
         int chargePlug = intent.getIntExtra(BatteryManager.EXTRA_PLUGGED, -1);
         boolean usbCharge = chargePlug == BatteryManager.BATTERY_PLUGGED_USB;
