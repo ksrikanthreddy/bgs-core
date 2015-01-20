d9bd9957cbdd90c90752aef46c206a633dadec46
Srikanth Reddy <kollisreekanth@gmail.com>
2015-01-20 14:13:38 +0530

    Update PowerConnectionReceiver.java

--- src/android/PowerConnectionReceiver.java
+++ src/android/PowerConnectionReceiver.java
@@ -43,7 +43,8 @@ public class PowerConnectionReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) { 
-        
+        Toast toast1 = Toast.makeText(context, "ON RECEIVE", Toast.LENGTH_SHORT);
+            toast1.show();
         try{
        	String batteryStatus = "";
        	IntentFilter ifilter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
