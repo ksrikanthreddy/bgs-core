149c9d41c2947417e60ed3ff425726869824fcec
Srikanth Reddy <kollisreekanth@gmail.com>
2015-01-21 14:32:08 +0530

    Update BackgroundServicePluginLogic.java

--- src/android/BackgroundServicePluginLogic.java
+++ src/android/BackgroundServicePluginLogic.java
@@ -12,6 +12,7 @@ import android.app.ActivityManager.RunningServiceInfo;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.IBinder;
 import android.os.RemoteException;
 import android.util.Log;
