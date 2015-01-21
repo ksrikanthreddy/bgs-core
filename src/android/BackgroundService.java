cb088cf878e323a2a02e39583e83d80f69ead519
Srikanth Reddy <kollisreekanth@gmail.com>
2015-01-21 20:15:24 +0530

    Update BackgroundService.java

--- src/android/BackgroundService.java
+++ src/android/BackgroundService.java
@@ -138,9 +138,7 @@ public abstract class BackgroundService extends Service {
 		// Done this to ensure that my initialisation code is called.
 		// Found that the onStart was not called if Android was re-starting the service if killed
 		initialiseService();
-		PowerConnectionReceiver batReceiver = new PowerConnectionReceiver();
-		IntentFilter ifilter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
-		this.registerReceiver(batReceiver, ifilter);
+		
 	}
 	
 	@Override  
@@ -309,6 +307,9 @@ public abstract class BackgroundService extends Service {
 				this.setupTimerTask();
 			
 			this.mServiceInitialised = true;
+			PowerConnectionReceiver batReceiver = new PowerConnectionReceiver();
+			IntentFilter ifilter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
+			this.registerReceiver(batReceiver, ifilter);
 		}
 
 	}
