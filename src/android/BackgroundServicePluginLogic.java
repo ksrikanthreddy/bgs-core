515e88022886e47ef89f5fb3703a3f0f62b5bcd5
Srikanth Reddy <kollisreekanth@gmail.com>
2015-01-21 14:35:43 +0530

    Update BackgroundServicePluginLogic.java

--- src/android/BackgroundServicePluginLogic.java
+++ src/android/BackgroundServicePluginLogic.java
@@ -103,6 +103,10 @@ public class BackgroundServicePluginLogic {
 	public BackgroundServicePluginLogic(Context pContext) {
 		this.mContext = pContext;
 	}
+	
+	public BackgroundServicePluginLogic() {
+		
+	}
 
 	/*
 	 ************************************************************************************************
