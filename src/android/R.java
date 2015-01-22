8f5f11eebddddd8231130f1a3350740ace87d50c
Srikanth Reddy <kollisreekanth@gmail.com>
2015-01-22 12:30:37 +0530

    Delete R.java

--- src/android/R.java
+++
@@ -1,9 +0,0 @@
-package com.red_folder.phonegap.plugin.backgroundservice;
-
-public final class R {
-    public static final class attr {
-    }
-    public static final class drawable {
-        public static final int icon=0x7f020000;
-    }
-}
